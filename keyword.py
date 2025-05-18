44477df2b5c6e89c26d5145ab24008e229789e77
Prachi1273 <119164120+Prachi1273@users.noreply.github.com>
2025-05-18 11:24:08 +0530

    Add files via upload

---
+++ keyword.py
@@ -0,0 +1,12 @@
+def Display(name, age, marks):
+	print("Name is : ",name)
+	print("Age is : ",age)
+	print("marks are : ",marks)
+
+def main():
+	Display(name = "Prachi",age = 19,marks = 95)
+	
+	Display(age = 20,name = "sanika",marks = 88)
+
+if __name__ == "__main__":
+	main()
