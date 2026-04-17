e19252d9793dc7c8e347395bd38dc76247ac61e9
Prachi1273 <119164120+Prachi1273@users.noreply.github.com>
2026-04-17 11:58:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
-# Change username if needed
+# Change this path to your system username
 BASE_DIR = "/home/user/Downloads"
 
 SCREENSHOTS_DIR = os.path.join(BASE_DIR, "Screenshots")
+
+# Log file path
+LOG_FILE = os.path.join(BASE_DIR, "organizer.log")
